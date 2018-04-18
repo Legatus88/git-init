32b7ee87bd288e6c804a7a15c34f95a7834bdcf2
Legatus88 <y1wkn8@gmail.com>
2018-04-18 14:54:36 +0400

    merge

--- lesson2.rb
+++ lesson2.rb
@@ -1,21 +1,17 @@
-#здесь важна последовательность?
 filename = ARGV[0] || 'movies.txt' 
 
 unless File.exist?(filename)
-		puts 'file doesn\'t exist'
-		exit
+  puts 'file doesn\'t exist'
+  exit
 end
 
-filename = File.open('movies.txt')
-filename.each do |line|
-	result = line.split('|')
-	if result[1].include? "Max"
-   		film_name = result[1]
-   		rate = result[7]   		
-   		x = (rate.to_f - 8.0) * 10.0
-#округление здесь уместно?
-   		stars = '*' * x.round
-   		puts film_name + ' ' + stars
-	end
+File.readlines('movies.txt').each do |line|
+  result = line.split('|')
+  if result[1].include? "Max"
+    film_name = result[1]
+    rate = result[7]   		
+    x = (rate.to_f - 8.0) * 10.0
+    stars = '*' * x.round
+    puts film_name + ' ' + stars
+  end
 end
-filename.close
--- movies_array.rb
+++ movies_array.rb
@@ -1,8 +1,6 @@
 white_list = ['The Divide', 'Matrix', 'Silent House', 'Saw']
 
-kino = ARGV[0]
-
-if white_list.include?(kino) 
-	puts kino + ' is a good movie'
-else puts kino + ' is not in my list'
+if white_list.include?(ARGV[0]) 
+	puts ARGV[0] + ' is a good movie'
+else puts ARGV[0] + ' is not in my list'
 end
